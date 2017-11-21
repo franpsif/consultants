61fb7438d318bcd4d08f24b14834ee006d3c2b4f
franpsif <franpsif@gmail.com>
2017-11-21 22:41:49 +0100

    Added the 'show more' functionality. Modified the theme in order to see better the cards. Modified the layout in the users component in order to see the rows better.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,14 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.sass']
 })
 export class UsersComponent implements OnInit {
-  usersList: User[];
+  usersList: User[] = [];
+  showedUserList: User[] = [];
+  usersShowedCount = 1;
+  usersToAddEachTime = 9;
 
   constructor(private userService: UsersService) { }
 
   ngOnInit() {
     this.userService.getUsersList().subscribe(() => {
       this.usersList = this.userService.usersList;
+      this.showMoreUsers();
     });
+  }
+
+  showMoreUsers() {
+    for (let index = this.usersShowedCount; index < this.usersShowedCount + this.usersToAddEachTime; index++) {
+      const userToAdd = this.usersList[index];
+      if (userToAdd) {
+        this.showedUserList.push(userToAdd);
+      }
+    }
+
+    this.usersShowedCount += this.usersToAddEachTime;
   }
 
 }
